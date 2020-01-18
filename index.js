da532d16c314b789632986d625b925af7f539925
unknown <i00jko@outlook.com>
2020-01-18 16:54:15 +0100

    Dodat webhook, config file i nexe

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
+const express = require('express');
 const tmi = require('tmi.js');
+const fs = require('fs');
+const bodyParser = require('body-parser')
+const app = express();
+
+const config = JSON.parse(fs.readFileSync('config.json'));
 
 const options = {
     options: {
-        debug: true
+        debug: config.twitch.debug
     },
     connection: {
         cluster: 'aws',
         reconnect: true,
     },
     identity: {
-        username: "discharge97",
-        password: "oauth:csmcnoo2jjs4d3ykdgqghcm8a7o630"
+        username: config.twitch.username,
+        password: config.twitch.oauth
     },
-    channels: ['discharge97']
+    channels: [config.twitch.username]
 };
 
+app.use(bodyParser.json());
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send("OK");
+});
+
+app.listen(config.webhook.port, () => console.log(`Webhook running on ${config.webhook.hostname}:${config.webhook.port}!`));
+
+
 const client = new tmi.client(options);
-
-client.connect();
-
+// client.connect();
 client.on('connected', (adress, port) => {
-    client.action('discharge97', "Hello, discharge97 is here!");
+    if (config.twitch.showJoinMessage) {
+        client.action(config.twitch.username, config.twitch.joinMessage);
+    }
 });
