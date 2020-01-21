b67ab7a43b8a79fafa9df9a69bd6fa663da3e4a2
unknown <i00jko@outlook.com>
2020-01-21 18:48:24 +0100

    dodat CORS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const tmi = require('tmi.js');
 const fs = require('fs');
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express();
 
 const config = JSON.parse(fs.readFileSync('config.json'));
@@ -22,17 +23,23 @@ const options = {
 };
 
 const client = new tmi.client(options);
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.json());
 app.post('/', (req, res) => {
     console.log(req.body);
-    client.say(config.twitch.channel, "<username> has donaited 5$! Thank you!");
+    // client.say(config.twitch.channel, "<username> has donaited 5$! Thank you!");
     res.send("OK");
 });
 
 app.listen(config.webhook.port, () => console.log(`Webhook running on ${config.webhook.hostname}:${config.webhook.port}!`));
 
 
-client.connect();
+// client.connect();
 client.on('connected', (adress, port) => {
     if (config.twitch.showJoinMessage) {
         client.action(config.twitch.channel, config.twitch.joinMessage);
